cc4ebd429d87b15665103ebc387da35ac99f0a59
Yashawini2006 <yashoofr@gmail.com>
2026-03-11 15:25:34 +0530

    UC1 implemented in BookMyStayApp

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,27 @@
+/**
+ * ============================================================
+ * MAIN CLASS – UseCase1HotelBookingApp
+ * ============================================================
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * @author Yashawini
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+
+    /**
+     * Application entry point
+     */
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("       BOOK MY STAY APP          ");
+        System.out.println("  Hotel Booking Management v1.0  ");
+        System.out.println("=================================");
+
+        System.out.println("Welcome to Book My Stay!");
+        System.out.println("Application started successfully.");
+    }
+}
